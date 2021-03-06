2589c41abfa9d4a2b5d919ea737e250c4cb526d8
Babak Tourani <babak4@yahoo.com>
2021-03-06 17:30:19 +0000

    [009] Secure S3 bucket
    This commit secures the S3 bucket which is used for the test. The aim is
    to be able to use the `aws_s3.tf` config file independently (alongside
    `main.tf`) to create the S3 bucket using Terraform.

--- infra/terraform/aws_s3.tf
+++ infra/terraform/aws_s3.tf
@@ -1,3 +1,14 @@
 resource "aws_s3_bucket" "data_load" {
   bucket = "babak4-test-convex"
+  acl = "private"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
+  versioning {
+      enabled = true
+    }
 }
